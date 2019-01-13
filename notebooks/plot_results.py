5fc3a3cc8837ecd5e0c5ed60f3b5c65cc21755d2
Cam Allen <csal@brown.edu>
2019-01-12 22:17:42 -0500

    Update plot script to include BusyTaxi5x5 results

--- notebooks/plot_results.py
+++ notebooks/plot_results.py
@@ -2,12 +2,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-skilledq = np.loadtxt('results/skilledqlearning/results.txt')
-random = np.loadtxt('results/random/results.txt')
+envname = 'busytaxi5x5'
+skilledq = np.loadtxt('results/'+envname+'/skilledqlearning/results.txt')
+random = np.loadtxt('results/'+envname+'/random/results.txt')
 t = np.arange(len(skilledq))
 plt.plot(t, skilledq, label='q-learning w/ skills')
 plt.plot(t, random, label='random w/ skills')
-plt.title('Taxi 5x5')
+plt.title(envname)
 plt.xlabel('timesteps')
 plt.ylabel('total reward')
 plt.legend()
