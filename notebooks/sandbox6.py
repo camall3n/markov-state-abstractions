11b51767668aa6aa8fa485251f7d35ee4cd2720b
Cam Allen <csal@brown.edu>
2019-01-31 16:22:11 -0500

    Clean up visualization script

--- notebooks/sandbox6.py
+++ notebooks/sandbox6.py
@@ -34,34 +34,25 @@ c0 = s0[:,0]*env._cols+s0[:,1]
 s1 = np.asarray(states[1:,:])
 a = np.asarray(actions)
 
-np.sum(np.all(s0==s1,axis=1))
-
 sigma = 0.1
 x0 = s0 + sigma * np.random.randn(n_samples,2)
 x1 = x0 + np.asarray(s1 - s0) + sigma/2 * np.random.randn(n_samples,2)
 # x1 = s1 + sigma * np.random.randn(n_samples,2)
 
 fig = plt.figure(figsize=(10,6))
-ax = fig.add_subplot(231)
-ax.scatter(x0[:,0],x0[:,1],c=c0, cmap='Set3')
-# plt.xlim(-1.5,1.5)
-# plt.ylim(-1.5,1.5)
-plt.xlabel('x')
-plt.ylabel('y')
-plt.xticks([])
-plt.yticks([])
-ax.set_title('states (t)')
-# plt.show()
+def plot_states(x, fig, subplot=111, colors=None, cmap=None, title=''):
+    ax = fig.add_subplot(subplot)
+    ax.scatter(x[:,0],x[:,1],c=colors, cmap=cmap)
+    # plt.xlim(-1.5,1.5)
+    # plt.ylim(-1.5,1.5)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.xticks([])
+    plt.yticks([])
+    ax.set_title(title)
 
-ax = fig.add_subplot(233)
-ax.scatter(x1[:,0],x1[:,1],c=c0, cmap='Set3')
-# plt.xlim(-1.5,1.5)
-# plt.ylim(-1.5,1.5)
-plt.xlabel('x')
-plt.ylabel('y')
-plt.xticks([])
-plt.yticks([])
-ax.set_title('states (t+1)')
+plot_states(x0, fig, subplot=231, colors=c0, cmap='Set3', title='states (t)')
+plot_states(x1, fig, subplot=233, colors=c0, cmap='Set3', title='states (t+1)')
 
 #% Entangle variables
 def entangle(x):
@@ -78,10 +69,6 @@ u1 = entangle(x1)
 
 ax = fig.add_subplot(232)
 ax.imshow(u0[-1])
-# plt.xlim(-1.5,1.5)
-# plt.ylim(-1.5,1.5)
-# plt.xlabel('u')
-# plt.ylabel('v')
 plt.xticks([])
 plt.yticks([])
 ax.set_title('observations (t)')
@@ -98,58 +85,53 @@ test_x1 = torch.as_tensor(u1[-n_samples//10:,:], dtype=torch.float32)
 test_a  = torch.as_tensor(a[-n_samples//10:], dtype=torch.int)
 test_c  = c0[-n_samples//10:]
 
+def score_rep(fnet, frame):
+    with torch.no_grad():
+        fnet.eval()
+        tx0 = torch.as_tensor(u0, dtype=torch.float32)
+        tx1 = torch.as_tensor(u1, dtype=torch.float32)
+        ta  = torch.as_tensor(a, dtype=torch.long)
+        z0 = fnet.phi(tx0)
+        z1 = fnet.phi(tx1)
+        z1_hat = fnet.fwd_model(z0, ta)
+        a_hat = fnet.inv_model(z0,z1)
 
-with torch.no_grad():
-    tx0 = torch.as_tensor(u0, dtype=torch.float32)
-    tx1 = torch.as_tensor(u1, dtype=torch.float32)
-    ta  = torch.as_tensor(a, dtype=torch.long)
-    z0 = fnet.phi(tx0)
-    z1 = fnet.phi(tx1)
-    z1_hat = fnet.fwd_model(z0, ta)
-    a_hat = fnet.inv_model(z0,z1)
+        inv_loss = fnet.compute_inv_loss(a_logits=a_hat, a=ta)
+        fwd_loss = fnet.compute_fwd_loss(z0, z1, z1_hat)
+    return z0, z1_hat, z1, frame, inv_loss, fwd_loss
 
-    inv_loss = fnet.compute_inv_loss(a_logits=a_hat, a=ta)
-    fwd_loss = fnet.compute_fwd_loss(z0, z1, z1_hat)
+z0, z1_hat, z1, frame, inv_loss, fwd_loss = score_rep(fnet, frame=0)
 
-ax = fig.add_subplot(234)
-inv_x = z0.numpy()[:,0]
-inv_y = z0.numpy()[:,1]
-inv_sc = ax.scatter(inv_x,inv_y,c=c0, cmap='Set3')
-plt.xlim([-1.1,1.1])
-plt.ylim([-1.1,1.1])
-plt.xlabel(r'$z_0$')
-plt.ylabel(r'$z_1$')
-plt.xticks([])
-plt.yticks([])
-ax.set_title(r'$\phi(x_t)$')
+def plot_rep(z, fig, subplot=111, colors=None, cmap=None, title=''):
+    ax = fig.add_subplot(subplot)
+    x = z.numpy()[:,0]
+    y = z.numpy()[:,1]
+    sc = ax.scatter(x,y,c=colors, cmap=cmap)
+    plt.xlim([-1.1,1.1])
+    plt.ylim([-1.1,1.1])
+    plt.xlabel(r'$z_0$')
+    plt.ylabel(r'$z_1$')
+    plt.xticks([])
+    plt.yticks([])
+    ax.set_title(title)
+    return ax, sc
 
-ax = fig.add_subplot(235)
-fwd_x = z1_hat.numpy()[:,0]
-fwd_y = z1_hat.numpy()[:,1]
-fwd_sc = ax.scatter(fwd_x,fwd_y,c=c0, cmap='Set3')
+_, inv_sc = plot_rep(z0, fig, subplot=234, colors=c0, cmap='Set3', title=r'$\phi(x_t)$')
+ax, fwd_sc = plot_rep(z1_hat, fig, subplot=235, colors=c0, cmap='Set3', title=r'$T(\phi(x_t),a_t)$')
+_, true_sc = plot_rep(z1, fig, subplot=236, colors=c0, cmap='Set3', title=r'$\phi(x_{t+1})$')
+
 tframe = ax.text(-0.25, .7, 'frame = '+str(0))
 tinv = ax.text(-0.75, .5, 'inv_loss = '+str(inv_loss.numpy()))
 tfwd = ax.text(-0.75, .3, 'fwd_loss = '+str(fwd_loss.numpy()))
-plt.xlim([-1.1,1.1])
-plt.ylim([-1.1,1.1])
-plt.xlabel(r'$z_0$')
-plt.ylabel(r'$z_1$')
-plt.xticks([])
-plt.yticks([])
-ax.set_title(r'$T(\phi(x_t),a_t)$')
 
-ax = fig.add_subplot(236)
-true_x = z1.numpy()[:,0]
-true_y = z1.numpy()[:,1]
-true_sc = ax.scatter(true_x,true_y,c=c0, cmap='Set3')
-plt.xlim([-1.1,1.1])
-plt.ylim([-1.1,1.1])
-plt.xlabel(r'$z_0$')
-plt.ylabel(r'$z_1$')
-plt.xticks([])
-plt.yticks([])
-ax.set_title(r'$\phi(x_{t+1})$')
+def update_plots(z0, z1_hat, z1, frame, inv_loss, fwd_loss):
+    inv_sc.set_offsets(z0.numpy())
+    fwd_sc.set_offsets(z1_hat.numpy())
+    true_sc.set_offsets(z1.numpy())
 
+    tframe.set_text('frame = '+str(frame))
+    tinv.set_text('inv_loss = '+str(inv_loss.numpy()))
+    tfwd.set_text('fwd_loss = '+str(fwd_loss.numpy()))
 
 def get_batch(x0, x1, a, batch_size):
     idx = np.random.choice(len(a), batch_size, replace=False)
@@ -172,40 +154,20 @@ def animate(i, n_inv_steps=10, n_fwd_steps=1, plot_every=10):
             tx0, tx1, ta = get_next_batch()
             fnet.train_batch(tx0, tx1, ta, model='fwd')
 
-    with torch.no_grad():
-        fnet.eval()
-        tx0 = torch.as_tensor(u0, dtype=torch.float32)
-        tx1 = torch.as_tensor(u1, dtype=torch.float32)
-        ta  = torch.as_tensor(a, dtype=torch.long)
-        z0 = fnet.phi(tx0)
-        z1 = fnet.phi(tx1)
-        z1_hat = fnet.fwd_model(z0, ta)
-        a_hat = fnet.inv_model(z0,z1)
-
-        inv_loss = fnet.compute_inv_loss(a_logits=a_hat, a=ta)
-        fwd_loss = fnet.compute_fwd_loss(z0, z1, z1_hat)
-
-        inv_sc.set_offsets(z0.numpy())
-        fwd_sc.set_offsets(z1_hat.numpy())
-        true_sc.set_offsets(z1.numpy())
-
-        tframe.set_text('frame = '+str(i))
-        tinv.set_text('inv_loss = '+str(inv_loss.numpy()))
-        tfwd.set_text('fwd_loss = '+str(fwd_loss.numpy()))
+    update_plots(*score_rep(fnet, frame=i))
 
 #%%
 live = True
 # live = False
 
-# --- Watch live ---
 if live:
     plt.waitforbuttonpress()
+
 ani = matplotlib.animation.FuncAnimation(fig, animate, frames=n_frames, interval=1, repeat=False)
+
 if live:
     plt.show()
-
-# --- Save video to file ---
-if not live:
+else:
     Writer = matplotlib.animation.writers['ffmpeg']
     writer = Writer(fps=15, metadata=dict(artist='Cam Allen'), bitrate=256)
-    ani.save('video_max_err_over_detached_dz_0.1.mp4', writer=writer)
+    ani.save('video.mp4', writer=writer)
