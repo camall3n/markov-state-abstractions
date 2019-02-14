06c3873f4ba6ecfe3787cf645c9f487ed9a6a4cd
Cam Allen <csal@brown.edu>
2019-02-14 17:10:00 -0500

    Add multi-dimensional entropy optimization script

--- notebooks/entropy-train-multi.py
+++ notebooks/entropy-train-multi.py
@@ -6,9 +6,12 @@ import torch
 
 from notebooks.phinet import PhiNet
 
-np.random.seed(0)
-torch.manual_seed(0)
-random.seed(0)
+seed = 0
+np.random.seed(seed)
+torch.manual_seed(seed)
+random.seed(seed)
+
+n_dims = 3
 
 sigma = 1.0e-2
 sigma_tch = torch.as_tensor(sigma)
@@ -26,15 +29,15 @@ def compute_entropy_torch(x):
     return torch.log(torch.tensor(x.shape[0]-1, dtype=torch.float32)) - 1/len(x) * torch.sum(torch.log(eps + torch.sum(K(dx), dim=1)-K(torch.tensor(0))), dim=0)
 
 N = 1000
-x = np.random.normal(0,1,N)
-h = np.log(eps+1*np.sqrt(2*np.pi*np.e))
-x_np = x
+x = [np.random.normal(0, (i+1), N) for i in range(n_dims)]
+h = [np.log(eps+(i+1)*np.sqrt(2*np.pi*np.e)) for i in range(n_dims)]
+x_np = np.stack(x,axis=1)
 x_tch = torch.as_tensor(x_np, dtype=torch.float32)
 print(compute_entropy_np(x_np))
 print(compute_entropy_torch(x_tch))
 print(h)
 
-#%% -------
+#%% -----
 
 class EntropyNet(PhiNet):
     def __init__(self, *args, **kwargs):
@@ -45,18 +48,18 @@ class EntropyNet(PhiNet):
         self.train()
         self.optimizer.zero_grad()
         z = self.phi(x)
-        loss = compute_entropy_torch(z)
+        loss = torch.mean(compute_entropy_torch(z))
         if ascend:
             loss = -loss
         loss.backward()
         self.optimizer.step()
         return loss
 
-net = EntropyNet(input_shape=1, n_latent_dims=1, n_hidden_layers=1, n_units_per_layer=32, lr=1e-3)
-print(net.phi(x_tch).mean())
+net = EntropyNet(input_shape=n_dims, n_latent_dims=n_dims, n_hidden_layers=1, n_units_per_layer=32, lr=1e-3)
+net.phi(x_tch).mean(dim=0)
 
 #%%
-n_updates = 50
+n_updates = 400
 
 losses = []
 reps = []
@@ -76,6 +79,8 @@ for i in range(n_updates):
 reps = torch.stack(reps).numpy()
 losses = torch.stack(losses).detach().numpy()
 
+#%%
+
 fig = plt.figure(figsize=(10,4))
 plt.plot(range(2*n_updates), losses)
 plt.ylabel('entropy')
@@ -83,18 +88,21 @@ plt.xlabel('n_updates')
 plt.vlines(n_updates,min(losses), max(losses), linestyles='dashed')
 plt.tight_layout()
 
+
 fig = plt.figure(figsize=(10,4))
 axes = fig.subplots(nrows=2, ncols=5, sharey=True, sharex=True)
 axes = axes.flatten()
 idx = np.arange(0,2*n_updates,2*n_updates//10)
 
 for i in range(len(axes)):
-    sns.distplot(reps[idx[i]], hist=False, ax=axes[i])
+    for dim in range(n_dims):
+        sns.distplot(reps[idx[i]][:,dim], hist=False, ax=axes[i], label=str(dim))
     if i >= len(axes)/2:
         axes[i].set_xlabel('z')
     axes[i].set_title(str(idx[i])+' updates')
+    axes[i].legend()
 axes[0].set_ylabel('p(z)')
-axes[0].set_ylabel('p(z)')
+axes[5].set_ylabel('p(z)')
 
 plt.tight_layout()
 plt.show()
