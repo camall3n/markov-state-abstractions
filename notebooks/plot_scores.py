d87e5f4024581332e752f4cd90551f1e74bf6292
Cam Allen <csal@brown.edu>
2019-05-11 15:41:59 -0400

    Update plot_scores script

--- notebooks/plot_scores.py
+++ notebooks/plot_scores.py
@@ -20,29 +20,34 @@ def load_experiment(tag):
     results = [read_log(log) for log in logs]
     keys = list(zip(agents, seeds))
     data = pd.concat(results, join='outer', keys=keys, names=['agent','seed']).sort_values(by='seed', kind='mergesort').reset_index(level=[0,1])
-    return data[data['episode']<=200]
+    return data[data['episode']<=100]
 
-labels = ['tag']
+labels = ['tag','name']
 experiments = [
-    # 'test-6x6-random'
+    ('test-6x6-random', 'random'),
     # 'test-6x6-dqn-true-state',
-    # 'test-6x6-dqn-phi-train',
-    # 'test-6x6-dqn-phi-factored',
-    # 'test-6x6-dqn-phi-nofac',
+    ('test-6x6-dqn-phi-train', 'DQN (end-to-end)'),
+    # ('test-6x6-dqn-phi-factored', '2-D factored'),
+    # ('test-6x6-dqn-phi-nofac', '2-D unfactored'),
     # 'exp11-3d-rep',
-    # 'exp12-10d-rep',
+    # ('exp12-10d-rep', '10-D pre-trained'),
     # 'exp13joint10d',
-    'exp14_true_state',
-    'exp15_pre_2d',
+    # ('exp14_true_state', '2-D true state'),
+    # 'exp15_pre_2d',
+    # ('exp16-onehot36-joint', '36-D one-hot true state'),
+    ('exp17-factored-phi', 'DQN (pre-trained abstraction)'),
+    # ('exp18-unfactored-phi', 'DQN (unfactored)'),
 ]
-data = pd.concat([load_experiment(e) for e in experiments], join='outer', keys=experiments, names=labels).reset_index(level=[0])
+data = pd.concat([load_experiment(e[0]) for e in experiments], join='outer', keys=experiments, names=labels).reset_index(level=[0,1])
 
-g = sns.relplot(x='episode', y='total_reward', kind='line', hue='tag', data=data, height=6, alpha=0.2,
+# plt.rcParams.update({'font.size': 22})
+g = sns.relplot(x='episode', y='reward', kind='line', hue='name', data=data, height=6, alpha=0.2,
     # units='trial', estimator=None,
     # col='tag', col_wrap=2,
     # legend=False
 )
 plt.subplots_adjust(top=0.9)
-g.fig.suptitle('Reward vs. Time')
+g.fig.suptitle('Mean episode reward vs. Time (6x6 Gridworld, 150 trials each)')
+# plt.rcParams.update({'font.size': 22})
 plt.savefig('results/foo.png')
 plt.show()
