9382ae60877eb06d8b35e4358442cbd5805f6c6e
Cam Allen <csal@brown.edu>
2020-10-17 16:33:24 -0400

    Add sensor to rearrange x-y positions

--- gridworlds/sensors.py
+++ gridworlds/sensors.py
@@ -8,6 +8,21 @@ class NullSensor:
     def observe(self, s):
         return s
 
+class RearrangeXYPositionsSensor:
+    """Rearrange discrete x-y positions to break smoothness
+    """
+    def __init__(self, shape):
+        self.shape = shape
+        coords = np.asarray([[[x, y] for y in range(shape[1])] for x in range(shape[0])])
+        self.encoding = np.random.permutation(coords.reshape(-1, 2)).reshape(coords.shape)
+
+    def observe(self, s):
+        try:
+            obs = np.asarray([self.encoding[tuple(state)] for state in s])
+        except TypeError:
+            obs = self.encoding[tuple(s)]
+        return obs
+
 class OffsetSensor:
     def __init__(self, offset):
         self.offset = offset
@@ -32,20 +47,26 @@ class ImageSensor:
     def __init__(self, range, pixel_density=1):
         assert isinstance(range, (tuple, list, np.ndarray))
         self.range = range
-        self.size = (pixel_density*range[0][-1],pixel_density*range[1][-1])
+        self.size = (pixel_density * range[0][-1], pixel_density * range[1][-1])
 
     def observe(self, s):
         assert s.ndim > 0 and s.shape[-1] == 2
-        if s.ndim==1:
+        if s.ndim == 1:
             s = np.expand_dims(s, axis=0)
         n_samples = s.shape[0]
-        digitized = scipy.stats.binned_statistic_2d(s[:,0],s[:,1],np.arange(n_samples), statistic='count', bins=self.size, range=self.range, expand_binnumbers=True)
+        digitized = scipy.stats.binned_statistic_2d(s[:, 0],
+                                                    s[:, 1],
+                                                    np.arange(n_samples),
+                                                    statistic='count',
+                                                    bins=self.size,
+                                                    range=self.range,
+                                                    expand_binnumbers=True)
         digitized = digitized[-1].transpose()
-        x = np.zeros([n_samples,self.size[0],self.size[1]])
+        x = np.zeros([n_samples, self.size[0], self.size[1]])
         for i in range(n_samples):
-            x[i,digitized[i,0]-1,digitized[i,1]-1] = 1
-        if n_samples==1:
-            x = x[0,:,:]
+            x[i, digitized[i, 0] - 1, digitized[i, 1] - 1] = 1
+        if n_samples == 1:
+            x = x[0, :, :]
         return x
 
 class ResampleSensor:
@@ -55,7 +76,7 @@ class ResampleSensor:
         self.order = order
 
     def observe(self, s):
-        return np.kron(s, np.ones((self.scale,self.scale)))
+        return np.kron(s, np.ones((self.scale, self.scale)))
 
 class BlurSensor:
     def __init__(self, sigma=0.6, truncate=1.0):
@@ -63,7 +84,10 @@ class BlurSensor:
         self.truncate = truncate
 
     def observe(self, s):
-        return scipy.ndimage.filters.gaussian_filter(s, sigma=self.sigma, truncate=self.truncate, mode='nearest')
+        return scipy.ndimage.filters.gaussian_filter(s,
+                                                     sigma=self.sigma,
+                                                     truncate=self.truncate,
+                                                     mode='nearest')
 
 class PairEntangleSensor:
     def __init__(self, n_features, index_a=None, index_b=None, amount=1.0):
@@ -75,9 +99,10 @@ class PairEntangleSensor:
         assert n_features > 1, 'n_features must be > 1'
         assert 0 <= amount and amount <= 1, 'amount must be between 0 and 1'
         self.n_features = n_features
-        self.rotation = np.pi/4 * amount
+        self.rotation = np.pi / 4 * amount
         self.rot_matrix = np.asarray([[np.cos(self.rotation), -1 * np.sin(self.rotation)],
-                                      [np.sin(self.rotation),      np.cos(self.rotation)]])
+                                      [np.sin(self.rotation),
+                                       np.cos(self.rotation)]])
         if index_b is not None:
             assert index_a is not None, 'Must specify index_a when specifying index_b'
             assert index_a != index_b, 'index_b cannot equal index_a (value {})'.format(index_a)
@@ -87,23 +112,25 @@ class PairEntangleSensor:
         else:
             self.index_a = np.random.randint(n_features)
         if index_b is None:
-            self.index_b = np.random.choice([i for i in range(n_features) if i!=self.index_a])
+            self.index_b = np.random.choice([i for i in range(n_features) if i != self.index_a])
 
     def observe(self, s):
         s_flat = np.copy(s).reshape(-1, self.n_features)
-        a = s_flat[:,self.index_a]
-        b = s_flat[:,self.index_b]
-        x = np.stack((a,b),axis=0)
+        a = s_flat[:, self.index_a]
+        b = s_flat[:, self.index_b]
+        x = np.stack((a, b), axis=0)
         x_rot = np.matmul(self.rot_matrix, x)
-        a, b = map(lambda a: np.squeeze(a, axis=0),np.split(x_rot, 2, axis=0))
-        s_flat[:,self.index_a] = a
-        s_flat[:,self.index_b] = b
+        a, b = map(lambda a: np.squeeze(a, axis=0), np.split(x_rot, 2, axis=0))
+        s_flat[:, self.index_a] = a
+        s_flat[:, self.index_b] = b
         return s_flat.reshape(s.shape)
 
 class PermuteAndAverageSensor:
     def __init__(self, n_features, n_permutations=1):
         self.n_features = n_features
-        self.permutations = [np.arange(n_features)]+[np.random.permutation(n_features) for _ in range(n_permutations)]
+        self.permutations = [np.arange(n_features)] + [
+            np.random.permutation(n_features) for _ in range(n_permutations)
+        ]
 
     def observe(self, s):
         s_flat = s.reshape(-1, self.n_features)
@@ -112,17 +139,19 @@ class PermuteAndAverageSensor:
             sp_flat = np.take(s_flat, p, axis=1)
             sp = sp_flat.reshape(s.shape)
             output += sp
-        return output/len(self.permutations)
+        return output / len(self.permutations)
 
 class TorchSensor:
     def __init__(self, dtype=torch.float32):
         self.dtype = dtype
+
     def observe(self, s):
         return torch.as_tensor(s, dtype=self.dtype)
 
 class UnsqueezeSensor:
     def __init__(self, dim=-1):
         self.dim = dim
+
     def observe(self, s):
         return s.unsqueeze(dim=self.dim)
 
--- notebooks/train_agent.py
+++ notebooks/train_agent.py
@@ -16,9 +16,9 @@ from gridworlds.agents.dqnagent import DQNAgent, FactoredDQNAgent
 from gridworlds.utils import reset_seeds, get_parser
 from gridworlds.sensors import *
 
-
 parser = get_parser()
 # parser.add_argument('-d','--dims', help='Number of latent dimensions', type=int, default=2)
+# yapf: disable
 parser.add_argument('-a','--agent', type=str, required=True,
                     choices=['random','dqn','fqn'], help='Type of agent to train')
 parser.add_argument('-n','--n_trials', type=int, default=1,
@@ -51,11 +51,9 @@ parser.add_argument('--no_sigma', action='store_true',
                     help='Turn off sensors and just use true state; i.e. x=s')
 parser.add_argument('-v','--video', action='store_true',
                     help='Show video of agent training')
-parser.set_defaults(save=False)
-parser.set_defaults(video=False)
-parser.set_defaults(no_phi=False)
-parser.set_defaults(train_phi=False)
-parser.set_defaults(no_sigma=False)
+parser.add_argument('--rearrange_xy', action='store_true',
+                    help='Rearrange discrete x-y positions to break smoothness')
+# yapf: enable
 args = parser.parse_args()
 if args.train_phi and args.no_phi:
     assert False, '--no_phi and --train_phi are mutually exclusive'
@@ -65,31 +63,35 @@ if args.video:
 
 log_dir = 'scores/' + str(args.tag)
 os.makedirs(log_dir, exist_ok=True)
-log = open(log_dir+'/scores-{}-{}.txt'.format(args.agent, args.seed), 'w')
+log = open(log_dir + '/scores-{}-{}.txt'.format(args.agent, args.seed), 'w')
 
 #%% ------------------ Define MDP ------------------
 env = GridWorld(rows=args.rows, cols=args.cols)
 gamma = 0.9
 
 #%% ------------------ Define sensor ------------------
-if args.no_sigma:
-    sensor = SensorChain([])
-else:
-    sensor = SensorChain([
-        OffsetSensor(offset=(0.5,0.5)),
+sensor_list = []
+if args.rearrange_xy:
+    sensor_list.append(RearrangeXYPositionsSensor((env._rows, env._cols)))
+if not args.no_sigma:
+    sensor_list += [
+        OffsetSensor(offset=(0.5, 0.5)),
         NoisySensor(sigma=0.05),
-        ImageSensor(range=((0,env._rows), (0,env._cols)), pixel_density=3),
+        ImageSensor(range=((0, env._rows), (0, env._cols)), pixel_density=3),
         # ResampleSensor(scale=2.0),
         BlurSensor(sigma=0.6, truncate=1.),
-    ])
+    ]
+sensor = SensorChain(sensor_list)
 
 #%% ------------------ Define abstraction ------------------
 if args.no_phi:
     phinet = NullAbstraction(-1, args.latent_dims)
 else:
     x0 = sensor.observe(env.get_state())
-    phinet = PhiNet(input_shape=x0.shape, n_latent_dims=args.latent_dims, n_hidden_layers=1,
-            n_units_per_layer=32)
+    phinet = PhiNet(input_shape=x0.shape,
+                    n_latent_dims=args.latent_dims,
+                    n_hidden_layers=1,
+                    n_units_per_layer=32)
     if args.phi_path:
         modelfile = 'models/{}/phi-{}.pytorch'.format(args.phi_path, args.seed)
         phinet.load(modelfile)
@@ -101,31 +103,66 @@ n_actions = 4
 if args.agent == 'random':
     agent = RandomAgent(n_actions=n_actions)
 elif args.agent == 'dqn':
-    agent = DQNAgent(n_features=args.latent_dims, n_actions=n_actions, phi=phinet, lr=args.learning_rate, batch_size=args.batch_size, train_phi=args.train_phi, gamma=gamma, factored=False)
+    agent = DQNAgent(n_features=args.latent_dims,
+                     n_actions=n_actions,
+                     phi=phinet,
+                     lr=args.learning_rate,
+                     batch_size=args.batch_size,
+                     train_phi=args.train_phi,
+                     gamma=gamma,
+                     factored=False)
 elif args.agent == 'fqn':
-    agent = DQNAgent(n_features=args.latent_dims, n_actions=n_actions, phi=phinet, lr=args.learning_rate, batch_size=args.batch_size, train_phi=args.train_phi, gamma=gamma, factored=True)
+    agent = DQNAgent(n_features=args.latent_dims,
+                     n_actions=n_actions,
+                     phi=phinet,
+                     lr=args.learning_rate,
+                     batch_size=args.batch_size,
+                     train_phi=args.train_phi,
+                     gamma=gamma,
+                     factored=True)
 elif args.agent == 'fqni':
-    agent = FactoredDQNAgent(n_features=args.latent_dims, n_actions=n_actions, phi=phinet, lr=args.learning_rate, batch_size=args.batch_size, train_phi=args.train_phi, gamma=gamma, factored=True)
+    agent = FactoredDQNAgent(n_features=args.latent_dims,
+                             n_actions=n_actions,
+                             phi=phinet,
+                             lr=args.learning_rate,
+                             batch_size=args.batch_size,
+                             train_phi=args.train_phi,
+                             gamma=gamma,
+                             factored=True)
 else:
     assert False, 'Invalid agent type: {}'.format(args.agent)
 
 #%% ------------------ Train agent ------------------
 if args.video:
-    fig, ax = plt.subplots(2,2)
+    fig, ax = plt.subplots(2, 2)
     ax = ax.flatten()
     fig.show()
 
     def plot_value_function(ax):
-        s = np.asarray([[np.asarray([x,y]) for x in range(args.cols)] for y in range(args.rows)])
+        s = np.asarray([[np.asarray([x, y]) for x in range(args.cols)] for y in range(args.rows)])
         v = np.asarray(agent.q_values(s).detach().numpy()).max(-1)
-        xy = OffsetSensor(offset=(0.5,0.5)).observe(s).reshape(args.cols, args.rows, -1)
-        ax.contourf(np.arange(0.5,args.cols+0.5), np.arange(0.5,args.rows+0.5), v, vmin=-10, vmax=0)
+        xy = OffsetSensor(offset=(0.5, 0.5)).observe(s).reshape(args.cols, args.rows, -1)
+        ax.contourf(np.arange(0.5, args.cols + 0.5),
+                    np.arange(0.5, args.rows + 0.5),
+                    v,
+                    vmin=-10,
+                    vmax=0)
 
     def plot_states(ax):
         data = pd.DataFrame(agent.replay.memory)
-        data[['x.r','x.c']] = pd.DataFrame(data['x'].tolist(), index=data.index)
-        data[['xp.r','xp.c']] = pd.DataFrame(data['xp'].tolist(), index=data.index)
-        sns.scatterplot(data=data, x='x.c',y='x.r', hue='done', style='done', markers=True, size='done', size_order=[1,0], ax=ax, alpha=0.3, legend=False)
+        data[['x.r', 'x.c']] = pd.DataFrame(data['x'].tolist(), index=data.index)
+        data[['xp.r', 'xp.c']] = pd.DataFrame(data['xp'].tolist(), index=data.index)
+        sns.scatterplot(data=data,
+                        x='x.c',
+                        y='x.r',
+                        hue='done',
+                        style='done',
+                        markers=True,
+                        size='done',
+                        size_order=[1, 0],
+                        ax=ax,
+                        alpha=0.3,
+                        legend=False)
         ax.invert_yaxis()
 
 for trial in tqdm(range(args.n_trials), desc='trials'):
@@ -166,7 +203,7 @@ for trial in tqdm(range(args.n_trials), desc='trials'):
             ax[2].plot(rewards, c='C3')
             ax[3].plot(losses, c='C1')
             # plot_states(ax[3])
-            ax[1].set_ylim([-10,0])
+            ax[1].set_ylim([-10, 0])
             fig.canvas.draw()
             fig.canvas.flush_events()
 
@@ -180,6 +217,6 @@ for trial in tqdm(range(args.n_trials), desc='trials'):
             'steps': step
         }
         json_str = json.dumps(score_info)
-        log.write(json_str+'\n')
+        log.write(json_str + '\n')
         log.flush()
 print('\n\n')
