645bf377eaaad2f58128e7cb75253064b3d6f338
Cam Allen <csal@brown.edu>
2020-11-15 23:51:48 -0500

    Remove older plot scripts

--- gridworlds/utils.py
+++ gridworlds/utils.py
@@ -19,27 +19,3 @@ def get_parser():
     which has a stupidly long argument that I always forget.
     """
     return argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
-def load_experiment(tag, coefs=None):
-    logfiles = sorted(glob.glob(os.path.join('logs', tag + '*', 'train-*.txt')))
-    seeds = [f.split('-')[-1].split('.')[0] for f in logfiles]
-    logs = [open(f, 'r').read().splitlines() for f in logfiles]
-
-    def read_log(log, coefs=coefs):
-        results = [json.loads(item) for item in log]
-        fields = results[0].keys()
-        data = dict([(f, np.asarray([item[f] for item in results])) for f in fields])
-        if coefs is None:
-            coefs = {
-                'L_inv': 1.0,
-            }
-        if 'L' not in fields:
-            data['L'] = sum([
-                coefs[f] * data[f] if f != 'L_fac' else coefs[f] * (data[f] - 1)
-                for f in coefs.keys()
-            ])
-        return data
-
-    results = [read_log(log) for log in logs]
-    data = dict(zip(seeds, results))
-    return data
