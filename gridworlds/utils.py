12e0b044ca6a2796d2bcec4d303bb3b01902e59d
Cam Allen <csal@brown.edu>
2020-11-15 23:22:17 -0500

    Remove taxi

--- gridworlds/utils.py
+++ gridworlds/utils.py
@@ -7,11 +7,6 @@ import random
 from sklearn.neighbors import KernelDensity
 import torch
 
-def manhattan_dist(pos1, pos2):
-    x1, y1 = pos1
-    x2, y2 = pos2
-    return np.abs(x2 - x1) + np.abs(y2 - y1)
-
 def reset_seeds(seed=0):
     np.random.seed(seed)
     random.seed(seed)
