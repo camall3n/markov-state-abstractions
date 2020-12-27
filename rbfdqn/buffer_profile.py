a8f259587a0762ec2e8240d216d9fa2a9ba79cb0
Cam Allen <csal@brown.edu>
2020-12-26 19:53:12 -0500

    Fix env.reset() bug in buffer_profile.py

--- rbfdqn/buffer_profile.py
+++ rbfdqn/buffer_profile.py
@@ -28,18 +28,6 @@ env_dict = create_env_dict(env)
 before_add = create_before_add_func(env)
 print(env_dict)
 
-# print("Deque as buffer")
-# buf = buffer_class.buffer_class(max_length=int(5e5),seed_number=0)
-# s = env.reset()
-# for i in range(int(10e5)):
-#     a = env.action_space.sample()
-#     ns, r, d, _ = env.step(a)
-#     buf.append(s, a, r, d, ns)
-#     s = ns
-#     if i != 0 and i % 1e5 == 0:
-#         print("Buffer size: {}".format(i))
-#         profile(buf)
-
 print("CPPRB as buffer")
 buf = ReplayBuffer(int(5e5), env_dict)
 s = env.reset()
@@ -48,6 +36,8 @@ for i in range(int(10e5)):
     ns, r, d, _ = env.step(a)
     buf.add(**before_add(obs=s, act=a, rew=r, done=d, next_obs=ns))
     s = ns
+	if d:
+		s = env.reset()
     if i != 0 and i % 1e5 == 0:
         print("Buffer size: {}".format(i))
         profile(buf)
