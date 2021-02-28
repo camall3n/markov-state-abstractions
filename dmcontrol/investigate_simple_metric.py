4548d793983c08d68cbb8eb3837eb656a9a81fac
Cam Allen <csal@brown.edu>
2021-02-28 17:49:40 -0500

    Remove references to dmcontrol

--- dmcontrol/investigate_simple_metric.py
+++ dmcontrol/investigate_simple_metric.py
@@ -1,111 +1,9 @@
-from tqdm import tqdm
-# ---------------------------------------------------------------
-# This hack prevents a matplotlib/dm_control crash on macOS.
-# We open/close a plot before importing dm_control.suite, which
-# in this case happens when we import gym and make a dm2gym env.
-import matplotlib.pyplot as plt
-plt.plot()
-plt.close()
 import gym
-# ---------------------------------------------------------------
-
-import imageio
+import matplotlib.pyplot as plt
 import numpy as np
-
-from dmcontrol import gym_wrappers as wrap
-from dmcontrol import rad
+from tqdm import tqdm
 
 #%%
-
-class DMCPendulum2D(gym.Wrapper):
-    """
-    Description:
-        Converts the default 3-D pendulum state (x, y, theta_dot) to a 2-D state (theta, theta_dot)
-
-    Usage:
-        Wraps a dm2gym Pendulum environment to get compressed state information
-    """
-    def reset(self, **kwargs):
-        state = self.env.reset(**kwargs)
-        return self.convert_state(state)
-
-    def step(self, action):
-        next_state, reward, done, info = self.env.step(action)
-        return self.convert_state(next_state), reward, done, info
-
-    @staticmethod
-    def convert_state(state):
-        theta_dot = state['velocity'][0]
-        y, x = state['orientation']
-        theta = np.arctan2(y, x)
-        return np.array([theta, theta_dot])
-
-
-class DMCPendulumEnergy(gym.Wrapper):
-    """
-    Description:
-        Converts a 3-D pendulum state (x, y, theta_dot) to a 2-D energy state (potential, kinetic)
-
-    Usage:
-        Wraps a dm2gym Pendulum environment to get compressed state information
-    """
-    def reset(self, **kwargs):
-        state = self.env.reset(**kwargs)
-        return self.convert_state(state)
-
-    def step(self, action):
-        next_state, reward, done, info = self.env.step(action)
-        return self.convert_state(next_state), reward, done, info
-
-    @staticmethod
-    def convert_state(state):
-        theta_dot = state['velocity'][0]
-        y, x = state['orientation']
-        mass = 1.0
-        gravity = 9.8
-        radius = 1.0
-        velocity = theta_dot * radius
-        potential_energy = 2 * mass * gravity * (y + 1)
-        kinetic_energy = 0.5 * mass * (velocity)**2
-        return np.array([potential_energy, kinetic_energy])
-
-class DMCPendulumAnnotated(gym.Wrapper):
-    """
-    Description:
-        Converts a 3-D pendulum state (x, y, theta_dot) to a 2-D energy state (potential, kinetic)
-
-    Usage:
-        Wraps a dm2gym Pendulum environment to get compressed state information
-    """
-    def reset(self, **kwargs):
-        state = self.env.reset(**kwargs)
-        return self.convert_state(state)
-
-    def step(self, action):
-        next_state, reward, done, info = self.env.step(action)
-        return self.convert_state(next_state), reward, done, info
-
-    @staticmethod
-    def convert_state(state):
-        theta_dot = state['velocity'][0]
-        y, x = state['orientation']
-        state = {
-            'x': x,
-            'y': y,
-            'theta': np.arctan2(y, x)/np.pi,
-            'theta_dot': theta_dot,
-        }
-        if state['theta'] > 1/2:
-            state['theta'] -= 2
-        mass = 1.0
-        gravity = 9.81
-        radius = 1.0
-        velocity = theta_dot * radius
-        state['potential_energy'] = 2 * mass * gravity * (y + 1)
-        state['kinetic_energy'] = 0.5 * mass * (velocity)**2
-        state['energy'] = state['potential_energy'] + state['kinetic_energy']
-        return state
-
 class Annotate(gym.Wrapper):
     """
     Description:
@@ -142,37 +40,8 @@ class Annotate(gym.Wrapper):
         state['energy'] = state['potential_energy'] + state['kinetic_energy']
         return state
 
-def wrap_env(env, feature_type='visual', size=(84, 84), action_repeat=1, frame_stack=3):
-    env = wrap.FixedDurationHack(env)
-    env = wrap.ObservationDictToInfo(env, "observations")
-
-    if feature_type == 'expert':
-        env = wrap.MaxAndSkipEnv(env, skip=action_repeat, max_pool=False)
-    else:
-        env = wrap.RenderOpenCV(env)
-        env = wrap.Grayscale(env)
-        env = wrap.ResizeObservation(env, size)
-        env = wrap.MaxAndSkipEnv(env, skip=action_repeat, max_pool=False)
-        env = wrap.FrameStack(env, frame_stack, lazy=False)
-    return env
-
-
-env = gym.make('dm2gym:PendulumSwingup-v0')#, environment_kwargs={'flat_observation': True})
-env = env.env
-# env = DMCPendulum2D(env)
-env = DMCPendulumEnergy(env)
-
-state = env.reset()
-state = env.step(env.action_space.sample())[0]
-img = env.render(mode='rgb_array', use_opencv_renderer=True)
-plt.imshow(img)
-
-plt.ion()
-plt.show()
-
 #%%
 env = gym.make('Pendulum-v0')#, environment_kwargs={'flat_observation': True})
-# env = env.env
 env = Annotate(env)
 state = env.reset()
 
@@ -204,24 +73,12 @@ for i in tqdm(range(n_episodes)):
     trajectories.append(trajectory)
 
 
-# t = trajectories[0]
-# pe, ke = t['potential_energy'], t['kinetic_energy']
-# plt.plot(pe+ke, '.-k', label='PE+KE')
-# plt.plot(pe, label='PE')
-# plt.plot(ke, '--', label='KE')
-# plt.xlabel('Time')
-# plt.ylabel('Energy')
-# plt.legend()
-# plt.show()
-
 plt.figure()
 for t in trajectories:
     plt.plot(t['kinetic_energy'], t['potential_energy'])
 plt.xlabel('KE')
 plt.ylabel('PE')
 plt.show()
-
-
 
 plt.figure()
 i = 0
