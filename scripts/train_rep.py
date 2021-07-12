39036203060ea41e621c7f0234b5f146f76b7564
Cam Allen <csal@brown.edu>
2021-07-12 16:47:39 -0400

    Move repvis.py from scripts/ -> markov_abstr/

--- scripts/train_rep.py
+++ scripts/train_rep.py
@@ -10,7 +10,7 @@ from tqdm import tqdm
 from markov_abstr.models.featurenet import FeatureNet
 from markov_abstr.models.autoencoder import AutoEncoder
 from markov_abstr.models.pixelpredictor import PixelPredictor
-from scripts.repvis import RepVisualization, CleanVisualization
+from markov_abstr.repvis import RepVisualization, CleanVisualization
 from gridworlds.domain.gridworld.gridworld import GridWorld, TestWorld, SnakeWorld, RingWorld, MazeWorld, SpiralWorld, LoopWorld
 from gridworlds.utils import reset_seeds, get_parser, MI
 from gridworlds.sensors import *
